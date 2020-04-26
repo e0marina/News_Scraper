346e123a2475d62e18fd6fa0639b4cf8fd8b5f06
Emily M <30880308+e0marina@users.noreply.github.com>
2020-04-25 19:17:22 -0700

    still setting up to push to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -27,16 +27,13 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
-
-// // Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/newsScraperDB", {
-//   useNewUrlParser: true,
-// });
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/newsScraperDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 ////////////////////////
 //Routes
